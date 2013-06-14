75d8f5075b2431d801cb8796812a7a144bfe34e5
biohzrdmx <biohazardmx@gmail.com>
2013-06-14 14:26:15 -0500

    Added js constants and basic html5 shimming

--- parts/header_html.php
+++ parts/header_html.php
@@ -4,6 +4,20 @@
 	<meta charset="UTF-8">
 	<title><?php echo $site->getPageTitle() ?></title>
 	<link rel="shortcut icon" href="<?php $site->urlTo('/favicon.ico', true) ?>">
+	<script type="text/javascript">
+		document.createElement('header');
+		document.createElement('hgroup');
+		document.createElement('nav');
+		document.createElement('menu');
+		document.createElement('section');
+		document.createElement('article');
+		document.createElement('aside');
+		document.createElement('footer');
+		var constants = {
+			siteUrl: '<?php $site->urlTo("", true) ?>',
+			ajaxUrl: '<?php $site->urlTo("/ajax.php", true) ?>'
+		};
+	</script>
 	<?php $site->includeStyles() ?>
 </head>
 <body class="<?php $site->bodyClass() ?>">
