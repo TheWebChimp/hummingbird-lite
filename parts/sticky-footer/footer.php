dede2f7e6c514683c3122937abeea0a658350cd3
biohzrdmx <biohazardmx@gmail.com>
2014-03-07 09:21:41 -0600

    Now with more HTML5 in your sticky-footer

--- parts/sticky-footer/footer.php
+++ parts/sticky-footer/footer.php
@@ -1,10 +1,8 @@
 		<div class="push"></div>
 	</div>
-	<div class="footer">
-		<footer class="boxfix-vert">
-			<div class="inner">
-				<hr>
-				<p>Copyright &copy; <?php echo date('Y'); ?></p>
-			</div>
-		</footer>
-	</div>
+	<footer class="boxfix-vert">
+		<div class="inner">
+			<hr>
+			<p>Copyright &copy; <?php echo date('Y'); ?></p>
+		</div>
+	</footer>
